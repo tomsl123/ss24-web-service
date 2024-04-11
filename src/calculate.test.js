46d69586122eb71e11d5f9b12e81ff911d36590f
David Linner <david.linner@smarthometeam.de>
2024-04-11 14:42:58 +0200

    Extended test cases for calculate

--- src/calculate.test.js
+++ src/calculate.test.js
@@ -1,6 +1,27 @@
 import {factorial} from './calculate';
 import {test, describe, expect} from "@jest/globals"; // this is optional, all three are global variables im runner scope
 
-test ('5! is 120', () => {
-    expect(factorial(5)).toBe(120)
+describe('factorial', () => {
+
+    test('5! is 120', () => {
+        expect(factorial(5)).toBe(120)
+    });
+
+    test('0! is 1', () => {
+        expect(factorial(0)).toBe(1)
+    });
+
+    test('Factorial of negative int is throwing exception ', () => {
+        expect(() => {
+            factorial(-5);
+        }).toThrow();
+    });
+
+})
+
+describe('product', ()=>{
+
+
+
 });
+
